e6ff2564a07b11f3675a3316e51672b0c02e43a4
CourtlandGentry <39118100+CourtlandGentry@users.noreply.github.com>
2020-08-05 16:43:41 +0800

    Add files via upload
    upload test files

---
+++ dlEicar.ps1
@@ -0,0 +1,6 @@
+﻿$url = "http://2016.eicar.org/download/eicar.com"
+$output = "$env:temp\eicar.com"
+$start_time = Get-Date
+
+Invoke-WebRequest -Uri $url -OutFile $output
+Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
