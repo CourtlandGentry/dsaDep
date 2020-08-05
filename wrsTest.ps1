f7c7ad8f4e6697da0ffa41509dcae75c6eccdb4d
CourtlandGentry <39118100+CourtlandGentry@users.noreply.github.com>
2020-08-05 17:35:21 +0800

    Delete wrsTest.ps1
    remove

--- wrsTest.ps1
+++
@@ -1,6 +0,0 @@
-$url = "http://ca02-1.winshipway.com/"
-$output = "$env:temp\winship.txt"
-$start_time = Get-Date
-
-Invoke-WebRequest -Uri $url -OutFile $output
-Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
